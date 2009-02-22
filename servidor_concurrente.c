909b1a7f7d535e10ac94b94b484db11a0027cf32
Andrés Mejía-Posada <wack-a-mole@scarlett.(none)>
2009-02-22 11:22:27 -0500

    Changed forks with threads

--- servidor_concurrente.c
+++ servidor_concurrente.c
@@ -3,10 +3,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/param.h>
+
+//constants
+#define MAXRUNS 1000
+
+/* Return codes */
+enum { RET_ACCEPTED, RET_TIMELIMIT_EXCEEDED, RET_RUNTIME_ERROR, RET_WRONG_ANSWER,
+       RET_PRESENTATION_ERROR, RET_COMPILE_ERROR, RET_INTERNAL_ERROR };
+
 
 char * premade_runs[11][3] = {
   //accepted
@@ -31,29 +41,38 @@ char * premade_runs[11][3] = {
   {"data/c.in", "data/c.out", "solutions/runtime_error.c"}
 };
 
-char buffer[MAXPATHLEN];
+//struct used to pass parameters to threads
+struct thread_parameters_t{
+  char input[MAXPATHLEN], output[MAXPATHLEN], source[MAXPATHLEN], buffer[MAXPATHLEN];
+  int run_id;
+};
 
-void process_run(int id, char input[], char output[], char source[]){
-  sprintf(buffer, "./verdugo %s %s %s", input, output, source);
-  struct timeval begin, end;
-  gettimeofday(&begin, NULL);
-  int ret = system(buffer);
-  if (ret == -1){
-    printf("Problem executing file: %s\n", strerror(errno));
-    return;
-  }
-  ret = WEXITSTATUS(ret);
-  gettimeofday(&end, NULL);
-  printf("\tRunning time: %lf\n",
-         end.tv_sec + end.tv_usec / 1000000.0
-         - begin.tv_sec - begin.tv_usec / 1000000.0);
-}
+//global variables
+char buffer[MAXPATHLEN]; //All-use buffer
+struct thread_parameters_t thread_parameters[MAXRUNS];
+pthread_t thread_ids[MAXRUNS];
+sem_t sem_screen; //Semaphore to avoid two threads writing to the screen at the same time
+
+//function prototypes
+void process_run(int id, char input[], char output[], char source[], char buffer[]);
+void * thread_main(void * arg);
+void print_verdict_message(int ret_code);
+int main(int argc, char * argv[]);
+
 
 int main(int argc, char * argv[]){
   struct timeval begin, end;
   int how_many, i;
+
   printf("How many runs do you want to run? ");
   scanf("%d", &how_many);
+  if (how_many < 0 || how_many > MAXRUNS){
+    fprintf(stderr, "ERROR: Number of runs must be between 0 and %d (both inclusive)\n", MAXRUNS);
+    exit(1);
+  }
+
+
+  sem_init(&sem_screen, 0, 1); //One screen available
 
   gettimeofday(&begin, NULL);
   for (i=0; i<how_many; ++i){
@@ -65,19 +84,77 @@ int main(int argc, char * argv[]){
     source = premade_runs[run_id][2];
 
     //atender_solicitud
-    if (fork() == 0){
-      process_run(i, input, output, source);
-      exit(0);
+    thread_parameters[i].run_id = i;
+    memcpy(thread_parameters[i].input, input, sizeof(thread_parameters[i].input));
+    memcpy(thread_parameters[i].output, output, sizeof(thread_parameters[i].output));
+    memcpy(thread_parameters[i].source, source, sizeof(thread_parameters[i].source));
+    int error_code = pthread_create(&thread_ids[i], NULL, thread_main, (void *) &thread_parameters[i]);
+    //pthread_detach(thread_ids[i]); /* Create a detached thread, so its memory is freed as soon as it ends */
+    //printf("Thread number %d has id = %lu. pthread_create returned %d\n", i, thread_ids[i], error_code);
+    if (error_code != 0){
+      thread_ids[i] = 0;
+      sprintf(buffer, "ERROR: Creating thread %d: ", i);
+      perror(buffer);
     }
   }
+
   for (i=0; i<how_many; ++i){
-    wait(NULL);
+    if (thread_ids[i]){
+        int error_code = pthread_join(thread_ids[i], NULL);
+        if (error_code != 0){
+          sprintf(buffer, "ERROR: pthread_join returned error code %d: \n", error_code);
+          perror(buffer);
+        }
+      }
+      }
+
+    gettimeofday(&end, NULL);
+    printf("Total running time: %lf\n",
+           end.tv_sec + end.tv_usec / 1000000.0
+           - begin.tv_sec - begin.tv_usec / 1000000.0);
+
+    return 0;
   }
-  gettimeofday(&end, NULL);
-  printf("Total running time: %lf\n",
-         end.tv_sec + end.tv_usec / 1000000.0
-         - begin.tv_sec - begin.tv_usec / 1000000.0);
 
-  return 0;
-}
 
+  void process_run(int id, char input[], char output[], char source[], char buffer[]){
+    sprintf(buffer, "./verdugo %s %s %s > /dev/null", input, output, source);
+    struct timeval begin, end;
+    gettimeofday(&begin, NULL);
+    int ret = system(buffer);
+    if (ret == -1){
+      printf("Problem executing file: %s\n", strerror(errno));
+      return;
+    }
+    ret = WEXITSTATUS(ret);
+    gettimeofday(&end, NULL);
+
+    /* lock the screen using the semaphore (i.e. do a P) */
+    sem_wait(&sem_screen);
+
+    printf("Run %d:\n", id);
+    print_verdict_message(ret);
+    printf("\tRunning time: %lf\n",
+           end.tv_sec + end.tv_usec / 1000000.0
+           - begin.tv_sec - begin.tv_usec / 1000000.0);
+
+    /* unlock the screen using the semaphore (i.e. do a V) */
+    sem_post(&sem_screen);
+  }
+
+
+  void * thread_main(void * arg){
+    struct thread_parameters_t * params = (struct thread_parameters_t *) arg;
+    /* printf("I am thread %d.\n" */
+    /*        "input = %s\n" */
+    /*        "output = %s\n" */
+    /*        "source = %s\n", params->run_id, params->input, params->output, params->source); */
+    process_run(params->run_id, params->input, params->output, params->source, params->buffer);
+
+    pthread_exit(NULL);
+  }
+
+  void print_verdict_message(int ret_code){
+    char * names[] = {"ACCEPTED", "TIMELIMIT EXCEEDED", "RUNTIME ERROR", "WRONG ANSWER", "PRESENTATION ERROR", "COMPILE ERROR", "INTERNAL ERROR"};
+    fprintf(stdout, "%d - %s\n", ret_code, names[ret_code]);
+  }
